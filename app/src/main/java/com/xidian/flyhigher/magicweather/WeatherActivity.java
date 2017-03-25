d0681db42ffcb724f05da0601260cdccc30a8643
fenghl <fenghualong1@126.com>
2017-03-25 19:41:54 +0800

    Third commit

--- app/src/main/java/com/xidian/flyhigher/magicweather/WeatherActivity.java
+++ app/src/main/java/com/xidian/flyhigher/magicweather/WeatherActivity.java
@@ -6,10 +6,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -18,6 +22,10 @@ public class WeatherActivity extends AppCompatActivity {
     private Button navButton;
     private Button shareButton;
     public TextView titleCity;
+    private ViewPager viewPager;
+    public ImageView[] pointViews;
+
+    private LinearLayout circle_linearLayout;
 
 
     @Override
@@ -29,6 +37,39 @@ public class WeatherActivity extends AppCompatActivity {
         navButton = (Button) findViewById(R.id.nav_button);
         shareButton = (Button) findViewById(R.id.share_button);
         titleCity = (TextView) findViewById(R.id.title_city);
+        circle_linearLayout = (LinearLayout) findViewById(R.id.circle);
+        // Find the view pager that will allow the user to swipe between fragments
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        // Create an adapter that knows which fragment should be shown on each page
+        WeatherFragmentPagerAdapter adapter = new WeatherFragmentPagerAdapter(getSupportFragmentManager(),this);
+        initPoint();
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrollStateChanged(int state){
+                Log.i("MainActivity","TEST:\n" +
+                        "state: " + state);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels){
+                Log.i("MainActivity","TEST:\n"
+                        + "posotion: " + position + "\n"
+                        + "positionOffset: " + positionOffset + "\n"
+                        + "positionOffsetPixels: " + positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position){
+                //pointViews[position].setImageResource(R.drawable.circle_dot);
+                for (int x = 0; x < pointViews.length; x++) {
+                    pointViews[x].setImageResource(R.drawable.circle_gray);
+                    if (x == position) {
+                        pointViews[x].setImageResource(R.drawable.circle_dot);;
+                    }
+                }
+            }
+        });
 
         navButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,12 +102,19 @@ public class WeatherActivity extends AppCompatActivity {
 
         });
 
-//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-//        String weatherString = prefs.getString("weather", null);
-//        final String weatherId;
-//        if (weatherString == null) {
-//            weatherId = getIntent().getStringExtra("weather_id");
-//            requestWeather(weatherId);
-//        }
+
+    }
+
+    private void initPoint() {
+        pointViews = new ImageView[3];        // 设置对应的小圆点
+        for (int x = 0; x < 3; x++) {
+            ImageView iv = new ImageView(this);
+            pointViews[x] = iv;
+            pointViews[x].setImageResource(R.drawable.circle_gray);
+            pointViews[x].setPadding(10, 0, 10, 0);
+            pointViews[x].setEnabled(false);
+            circle_linearLayout.addView(pointViews[x]);
+        }
+        pointViews[0].setImageResource(R.drawable.circle_dot);
     }
 }
